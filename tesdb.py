a2cb301ed8e7f73ce16fcd97b146c011bd746e90
zidny-z <azzidti34@gmail.com>
2020-12-25 15:15:02 +0700

    tes

--- tesdb.py
+++ tesdb.py
@@ -2,8 +2,7 @@ import sqlite3
 con = sqlite3.connect('database1.db')
 cursor = con.cursor()
  
-Query = "Insert into player(PlayerName,PlayerPassword,PlayerWeapon) Values('adminwkwk','adminwkwk','1')"
+Query = "Insert into player(PlayerName,PlayerPassword,PlayerWeapon) Values('adminwksswk','adminsswkwk','1')"
 # (\'%s\', \'%s\');'
-# Query = Query  % ('admin', 'admin')
 cursor.execute(Query)
 con.commit()
